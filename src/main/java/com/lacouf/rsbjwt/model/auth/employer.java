b6f163be9a71745999773705f274f7599071b850
XavierL6 <popfizz10@outlook.com>
2026-09-10 15:49:06 -0400

    employer class

---
+++ src/main/java/com/lacouf/rsbjwt/model/auth/employer.java
@@ -0,0 +1,30 @@
+package com.lacouf.rsbjwt.model.auth;
+
+import com.lacouf.rsbjwt.model.UserApp;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class employer extends UserApp {
+    @Column(name = "company_name")
+    private String companyName;
+
+    @Column(name = "activity_sector")
+    private String activitySector;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    public employer(String firstName, String lastName, Credentials credentials, String companyName, String activitySector, String phoneNumber) {
+        super(firstName, lastName, credentials);
+        this.activitySector = activitySector;
+        this.companyName = companyName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public employer() {
+
+    }
+}
