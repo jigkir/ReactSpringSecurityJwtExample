fcaf3847b337a48c3e225ab2121e7c5e996312a6
DohaELKHOU <dohaelk005@gmail.com>
2026-09-10 21:14:52 -0400

    Student registration validations and REST exceptions handler added

--- src/main/java/com/lacouf/rsbjwt/presentation/StudentController.java
+++ src/main/java/com/lacouf/rsbjwt/presentation/StudentController.java
@@ -3,6 +3,7 @@ package com.lacouf.rsbjwt.presentation;
 import com.lacouf.rsbjwt.service.StudentService;
 import com.lacouf.rsbjwt.service.dto.StudentRegistrationDto;
 import com.lacouf.rsbjwt.service.dto.UserResponseDto;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,7 +21,7 @@ public class StudentController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<UserResponseDto> save(@RequestBody StudentRegistrationDto studentRegistrationDto) {
+    public ResponseEntity<UserResponseDto> save(@Valid @RequestBody StudentRegistrationDto studentRegistrationDto) {
         UserResponseDto registeredStudent = studentService.save(studentRegistrationDto);
 
         return new ResponseEntity<>(registeredStudent, HttpStatus.CREATED);
--- src/main/java/com/lacouf/rsbjwt/service/dto/StudentRegistrationDto.java
+++ src/main/java/com/lacouf/rsbjwt/service/dto/StudentRegistrationDto.java
@@ -1,6 +1,29 @@
 package com.lacouf.rsbjwt.service.dto;
 
 import com.lacouf.rsbjwt.model.Discipline;
+import jakarta.validation.constraints.*;
 
-public record StudentRegistrationDto(String firstName, String lastName, String studentId, String email, String password, Discipline discipline) {
+public record StudentRegistrationDto(
+        @NotBlank
+        @Size(min = 2, max = 50)
+        String firstName,
+
+        @NotBlank
+        @Size(min = 2, max = 50)
+        String lastName,
+
+        @NotBlank
+        @Pattern(regexp = "^[0-9]*$", message = "student ID must contain only digits")
+        String studentId,
+
+        @NotBlank
+        @Email
+        String email,
+
+        @NotBlank
+        @Size(min = 8, max = 50)
+        String password,
+
+        @NotNull
+        Discipline discipline) {
 }
