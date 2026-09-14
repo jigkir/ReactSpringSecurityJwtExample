764af6fc1ccd7fb2fbbd7f66d71645e12140a663
XavierL6 <popfizz10@outlook.com>
2026-09-14 16:28:30 -0400

    Ajout de restriction sur Validateurs + Trim

--- src/main/java/com/lacouf/rsbjwt/service/dto/EmployerRegistrationDto.java
+++ src/main/java/com/lacouf/rsbjwt/service/dto/EmployerRegistrationDto.java
@@ -2,6 +2,7 @@ package com.lacouf.rsbjwt.service.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record EmployerRegistrationDto (
@@ -18,7 +19,7 @@ public record EmployerRegistrationDto (
         String email,
 
         @NotBlank
-        @Size(min = 8, max = 50, message = "Le mot de passe doit contenir entre 8 et 50 caractères")
+        @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).+$", message = "password must contain at least one digit, one lowercase, one uppercase, and one special character")
         String password,
 
         @NotBlank
@@ -26,9 +27,37 @@ public record EmployerRegistrationDto (
         String companyName,
 
         @NotBlank
+        @Size(min = 2, max = 30, message = "Le secteur d'activité doit contenir entre 2 et 30 caractères")
         String activitySector,
 
         @NotBlank
+        @Size(min = 10, max = 20, message = "Le numéro de téléphone doit contenir entre 10 et 20 caractères")
         String phoneNumber
 ) {
+
+        public EmployerRegistrationDto {
+                if (firstName != null) {
+                        firstName = firstName.trim();
+                }
+
+                if (lastName != null) {
+                        lastName = lastName.trim();
+                }
+
+                if (email != null) {
+                        email = email.trim().toLowerCase();
+                }
+
+                if (companyName != null) {
+                        companyName = companyName.trim();
+                }
+
+                if (activitySector != null) {
+                        activitySector = activitySector.trim();
+                }
+
+                if (phoneNumber != null) {
+                        phoneNumber = phoneNumber.trim();
+                }
+        }
 }
