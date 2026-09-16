51e1656fce1904d01694a9340850bff76422a0f4
DohaELKHOU <dohaelk005@gmail.com>
2026-09-15 20:20:56 -0400

    Password regex fixed to forbid whitespaces

--- src/main/java/com/lacouf/rsbjwt/service/dto/StudentSignUpDto.java
+++ src/main/java/com/lacouf/rsbjwt/service/dto/StudentSignUpDto.java
@@ -24,7 +24,7 @@ public record StudentSignUpDto(
 
         @NotBlank
         @Size(min = 8, max = 50)
-        @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).+$", message = "password must contain at least one digit, one lowercase, one uppercase, and one special character")
+        @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])\\S+$", message = "password must contain at least one digit, one lowercase, one uppercase, one special character (@#$%^&+=), and must not contain whitespace")
         String password,
 
         @NotNull
