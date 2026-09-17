e8694a860333f13f53870f3f025849e03e1ea39e
jigkir <leotran1460740@gmail.com>
2026-09-17 15:09:20 -0400

    Refactor CommonFields: update PASSWORD_REGEX to disallow whitespace

--- react-vite-project/src/utils/CommonFields.jsx
+++ react-vite-project/src/utils/CommonFields.jsx
@@ -20,7 +20,7 @@ export const RoleField = ({
 );
 
 export const EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-export const PASSWORD_REGEX = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!]).+$/;
+export const PASSWORD_REGEX = /^(?!.*\s)(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!]).+$/;
 
 export const handleSpaceKeyDown = (e) => {
     if (e.key === ' ') {
