84d8bfe2f97c963990fe088fe626845d6c88e592
DohaELKHOU <dohaelk005@gmail.com>
2026-09-17 14:37:26 -0400

    Slash added to path for lisibility

--- src/main/java/com/lacouf/rsbjwt/presentation/EmployerController.java
+++ src/main/java/com/lacouf/rsbjwt/presentation/EmployerController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/employer")
+@RequestMapping("/api/employer")
 public class EmployerController {
     private final EmployerService employerService;
 
