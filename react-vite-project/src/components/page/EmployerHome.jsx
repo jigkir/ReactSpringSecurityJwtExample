b99ff5af3c5eaea43ae90eca0f5b47db8ea73cfc
Soumeya T-C <soumeya.tahar@gmail.com>
2026-09-21 23:18:07 -0400

    clean up

--- react-vite-project/src/components/page/EmployerHome.jsx
+++ react-vite-project/src/components/page/EmployerHome.jsx
@@ -3,7 +3,6 @@ import { useOutletContext } from "react-router-dom";
 import fetcher from "../../utils/fetcher.js";
 import {useTranslation} from 'react-i18next';
 
-
 const EmployerHome = ({user}) => {
     const [message, setMessage] = useState("");
     const {dark} = useOutletContext();
